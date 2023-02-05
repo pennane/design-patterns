bb3d07f641fd24c685714bcf5922ea5d1f0778e9
Arttu Pennanen <arttu@pennanen.org>
2023-02-05 13:48:27 +0200

    composite explanation

--- design-patterns/src/composite/Main.java
+++ design-patterns/src/composite/Main.java
@@ -2,6 +2,17 @@ package composite;
 
 public class Main {
 	public static void main(String[] args) {
+		/**
+		 * Decided against adding add and remove methods to the game as I could not find
+		 * a clean way to support them.
+		 * 
+		 * (i.e. I did not want to do any typecasting or adding redundant methods to the
+		 * leaf classes)
+		 * 
+		 * This solution is clean in the sense that there are no methods :D Just using
+		 * the constructors to create IMMUTABLE datastructures.
+		 */
+
 		IComponent prosessori = new Cpu();
 		IComponent näytönohjain = new Gpu();
 		IComponent ramMuisti = new Ram();
@@ -10,9 +21,9 @@ public class Main {
 		IComponent ärGeeBeeLedit = new RgbLedThing();
 
 		IComponent emolevy = new Motherboard(prosessori, näytönohjain, ramMuisti, ssd, poweri);
-		
+
 		IComponent kotelo = new Case(emolevy, ärGeeBeeLedit);
-		
+
 		IComponent tietokone = new Computer(kotelo);
 
 		System.out.println(tietokone.getPrice());
